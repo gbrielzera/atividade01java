f6597e5f3bad0410f31bf4720fbc740aeb1b1da4
GabrielCezar <cezaaar57@gmail.com>
2025-12-12 16:54:19 -0300

    Criando primeira interface .FXML pelo SceneBuilder.

--- src/main/java/main/java/com/gabriel/piano/Main.java
+++ src/main/java/main/java/com/gabriel/piano/Main.java
@@ -1,8 +1,25 @@
 package main.java.com.gabriel.piano;
-import static javafx.application.Application.launch;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // Carrega o FXML
+        Parent root = FXMLLoader.load(getClass().getResource("/com/gabriel/piano/piano.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Mini Piano");
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        launch(JavaFX.class, args);
+        launch(args); // inicia o JavaFX
     }
 }
---
+++ src/main/java/main/java/com/gabriel/piano/SuaTelaController.java
@@ -0,0 +1,18 @@
+package main.java.com.gabriel.piano;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class SuaTelaController {
+
+    @FXML
+    private Button meuBotao;
+
+    @FXML
+    private Label minhaLabel;
+
+    @FXML
+    public void initialize() {
+        // tuas tretas
+    }
+}
